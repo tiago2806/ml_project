cdeedc7a7b352ec5a60ecc9b9fa579aca0fe0712
Tiago <tiagohjcarvalho@gmail.com>
2026-05-19 16:06:41 +0100

    class distance-based clustering

--- src/datamodelling.py
+++ src/datamodelling.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.metrics import silhouette_score
+from utils import (
+    plot_dendrogram
+)
 
 class Clusteringworkflow:
 
@@ -10,23 +14,64 @@ class Clusteringworkflow:
         self.algorithm = algorithm
 
         
-    def elbow_method(self, X, max_k = 25):
-        if  not isinstance(self.algorithm, KMeans):
-            raise ValueError("Elbow method is only for KMeans algorithm.")        
+    def find_optimal_k(self, data_scaled, K_range):
+        if isinstance(self.algorithm, KMeans):
+            dispersion = []
+            for k in range(2, K_range):
+                model_kmeans = KMeans(n_clusters=k, random_state=0).fit(data_scaled) #experimental fit
+                dispersion.append(model_kmeans.inertia_)
 
-        dispersion = []
-        for k in range(2, max_k):
-            model = self.algorithm.fit(X)
-            dispersion.append(model.inertia_)
+            plt.plot(range(2, K_range), dispersion, marker='o')
+            plt.xlabel('Number of clusters')
+            plt.ylabel('Dispersion (inertia)')
+            plt.show()
 
-        plt.plot(range(1, max_k), dispersion, marker='o')
+        elif isinstance(self.algorithm, AgglomerativeClustering):
+            model_hierarchical = self.algorithm
+            fig, ax = plt.subplots()
+            plt.title("Hierarchical Clustering Dendrogram")
+            # plot the top three levels of the dendrogram
+            plot_dendrogram(model_hierarchical, truncate_mode="level", p=50)
+            plt.axhline(y = 50, color = 'r', linestyle = '-')
+            plt.show() 
+
+        else:
+            raise ValueError("This is only for KMeans or AgglomerativeClustering")
+        
+    def silhouette(self, data_scaled, K_range):
+        silhouette_scores = []
+
+        for k in K_range:
+            if isinstance(self.algorithm, KMeans):
+                model_kmeans = KMeans(n_clusters=k, random_state=0).fit(data_scaled)
+                labels = model_kmeans.labels_
+            elif isinstance(self.algorithm, AgglomerativeClustering):
+                model_hierarchical = AgglomerativeClustering(n_clusters=k).fit(data_scaled)
+                labels = model_hierarchical.labels_
+            else:
+                raise ValueError("Silhouette only works for KMeans or AgglomerativeClustering")
+            
+            silhouette_scores.append(silhouette_score(data_scaled, labels))
+
+
+        plt.figure(figsize=(8, 5))
+        plt.plot(K_range, silhouette_scores, marker='o')
         plt.xlabel('Number of clusters')
-        plt.ylabel('Dispersion (inertia)')
+        plt.ylabel('Silhouette Score')
+        plt.title('Silhouette Scores')
         plt.show()
     
-    def fit(self, X):
-        dispersion = []
-        
-        self.algorithm.fit(X)
+    def fit(self, data_scaled):
+        self.algorithm.fit(data_scaled) #fit the model to the data with the correct number of k
 
         return self
+
+    def predict(self, X_train, data_scaled):
+        if not isinstance(self.algorithm, KMeans):
+            raise ValueError("Predict only works for KMeans")
+
+        X_train['cluster_kmeans'] = self.algorithm.predict(data_scaled) 
+    
+
+
+    
---
+++ src/utils.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.metrics import silhouette_score
+
+def plot_dendrogram(model, **kwargs):
+    '''
+    Create linkage matrix and then plot the dendrogram
+    Arguments: 
+    - model(HierarchicalClustering Model): hierarchical clustering model.
+    - **kwargs
+    Returns:
+    None, but dendrogram plot is produced.
+    '''
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
